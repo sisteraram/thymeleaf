52785206f6bb7c4f523e738959610825db906f0f
sisteraram <sisteraram@naver.com>
2018-11-14 11:15:26 +0900

    first

---
+++ b3-2/src/main/java/org/zerock/B32Application.java
@@ -0,0 +1,12 @@
+package org.zerock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B32Application.class, args);
+	}
+}
