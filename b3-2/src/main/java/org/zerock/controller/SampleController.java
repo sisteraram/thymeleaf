cc6c83172c6b050e305256aa80413845ee6202ff
sisteraram <sisteraram@naver.com>
2018-11-14 13:17:58 +0900

    fourth

--- b3-2/src/main/java/org/zerock/controller/SampleController.java
+++ b3-2/src/main/java/org/zerock/controller/SampleController.java
@@ -45,75 +45,14 @@ public class SampleController {
 		model.addAttribute("arr", new String[] {"AAA", "BBB", "CCC", "DDD"});
 
 	}
-
-	@GetMapping("/sample4")
-	public void sample4(Model model) {
-
-		List<MemberVO> list = new ArrayList<>();
-
-		for (int i = 0; i < 10; i++) {
-
-			list.add(new MemberVO(i, "u000" + i % 3, "p0000" + i % 3, "홍길동" + i,
-					new Timestamp(System.currentTimeMillis())));
-		}
-
-		model.addAttribute("list", list);
-
-	}
-
-	@GetMapping("/sample5")
-	public void sample5(Model model) {
-
-		String result = "SUCCESS";
-
-		model.addAttribute("result", result);
-
-	}
-
-	@GetMapping("/sample6")
-	public void sample6(Model model) {
-
-		List<MemberVO> list = new ArrayList<>();
-
-		for (int i = 0; i < 10; i++) {
-
-			list.add(new MemberVO(i, "u0" + i, "p0" + i, "홍길동" + i,
-					new Timestamp(System.currentTimeMillis())));
-		}
-
-		model.addAttribute("list", list);
-
-		String result = "SUCCESS";
-
-		model.addAttribute("result", result);
-
-	}
 	
 	
-	@GetMapping("/sample7")
-	public void sample7(Model model) {
-
-		model.addAttribute("now", new Date());
-		model.addAttribute("price", 123456789);
-		model.addAttribute("title", "This is a just sample.");
-		model.addAttribute("options", Arrays.asList("AAAA","BBB","CCC","DDD"));
+	@GetMapping("/test1")
+	public void test1Page() {
 		
 	}
 	
-	@GetMapping("/sample8")
-	public void sample8(Model model) {
-		
-	}
 	
-	@GetMapping("/sample9")
-	public void sample9(Model model) {
-		
-	}
-	
-	@GetMapping("/sample/hello")
-	public void hello(){
-		
-	}
-	
+
 
 }
